1ad8a8a0ad1c872654d7c12d86c588b267773a2f
Patrick <patrick.pusswald@gmail.com>
2023-03-06 15:35:39 +0100

    added Authorization to the report Error

--- src/app/report-service.service.ts
+++ src/app/report-service.service.ts
@@ -1,5 +1,6 @@
+import { UserDataService } from './user/userData.service';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map, retry } from 'rxjs/operators';
 import { Report } from './dialog/classes/Report';
@@ -17,16 +18,22 @@ export class ReportServiceService {
 
   //adds a new Skriptreport to the Database
   addSkript(report: ReportSkript): Observable<any> {
+    let token = this.userData.token;
+    let headers = new HttpHeaders().set('Authorization', token);
     console.log("Skriptreport sent to backend!");
-    return this.http.post<any>(this.skriptReportUrl, report, {responseType: 'json'});
+    return this.http.post<any>(this.skriptReportUrl, report, {responseType: 'json',
+                                                              headers: headers});
   }
 
   //adds a new Videoreport to the Database
   addVideo(report: ReportVideo): Observable<any> {
+    let token = this.userData.token;
+    let headers = new HttpHeaders().set('Authorization', token);
     console.log("Videoreport sent to backend!");
-    return this.http.post<any>(this.videoReportUrl, report, {responseType: 'json'});
+    return this.http.post<any>(this.videoReportUrl, report, {responseType: 'json',
+                                                            headers: headers});
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private userData: UserDataService) { }
 
 }
