3fcee6182896b9fcaecf51d6e77c0898c34974c0
Patrick <patrick.pusswald@gmail.com>
2023-03-12 11:17:22 +0100

    class VideoPost added correct attributes

--- src/app/interfaces/videoPost.ts
+++ src/app/interfaces/videoPost.ts
@@ -1,4 +1,4 @@
-export class SkriptPost {
+export class VideoPost {
 
     reportType!: any;
     module!: any;
@@ -7,25 +7,25 @@ export class SkriptPost {
     priority!: any; 
     author!: any;
     eMail!: any;
-    page!: any;
-    chapter!: any;
-    illustrationNumber!: any; 
-    tableNumber!: any;
+    videoTitle!: any;
+    timestampStart!: any;
+    timestampEnd!: any; 
+    videoURL!: any;
     report_date!: any; 
     granted_date: any = 0;
     completed_date: any = 0;
     mitify_user_id!: any;
 
-        constructor(reportType: any, module: any, description: any, status: any, priority: any, page: any, chapter: any, illustrationNumber: any, tableNumber: any, report_date: any, mitify_user_id: any, author: any, eMail: any) {
+        constructor(reportType: any, module: any, description: any, status: any, priority: any, videoTitle: any, timestampStart: any, timestampEnd: any, videoURL: any, report_date: any, mitify_user_id: any, author: any, eMail: any) {
             this.reportType = reportType;
             this.module = module;
             this.description = description;
             this.status = status;
             this.priority = priority;
-            this.page = page;
-            this.chapter = chapter;
-            this.illustrationNumber = illustrationNumber;
-            this.tableNumber = tableNumber;
+            this.videoTitle = videoTitle;
+            this.timestampStart = timestampStart;
+            this.timestampEnd = timestampEnd;
+            this.videoURL = videoURL;
             this.report_date = report_date;
             this.mitify_user_id =mitify_user_id;
             this.author = author;
